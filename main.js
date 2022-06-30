95cda70565f75d47f2e7d6b685844f2ac7ba7dd7
royrub25 <76081193+royrub25@users.noreply.github.com>
2022-06-30 10:17:47 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+var status = " ";
+
+
+function setup()
+{
+    canvas = createCanvas(480, 380);
+    canvas.center();
+}
+function setup() {
+    canvas = createCanvas(480, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+  }
+
+function draw()
+{
+    image(video, 0, 0, 480, 380);
+}
+
+function start()
+{
+    objectDetector - ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
